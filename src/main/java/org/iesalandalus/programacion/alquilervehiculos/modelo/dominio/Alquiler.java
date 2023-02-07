c0c52c93135062f63734c353618c3e0a43a859f8
Glootie-MGF <116271650+Glootie-MGF@users.noreply.github.com>
2023-02-07 14:17:35 +0100

    Implementación de la clase Alquiler

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Alquiler.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Alquiler.java
@@ -1,8 +1,12 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+import javax.naming.OperationNotSupportedException;
 
 public class Alquiler {
 
@@ -30,7 +34,26 @@ public class Alquiler {
 		setCliente(alquiler.getCliente());
 	}
 	//Métodos
+	public void devolver(LocalDate fechaDevolucion) throws OperationNotSupportedException {
+		//se encargará de asignar la fecha de devoluión si ésta es correcta
+		if (this.fechaDevolucion != null) {
+			throw new OperationNotSupportedException("ERROR: ya tiene fecha de devolución.");
+		}
+		setFechaDevolucion(fechaDevolucion);
+	}
 	
+	public int getPrecio() {
+		//devolverá el precio del alquiler conforme a la fórmula establecida por nuestro cliente
+		//(precioDia + factorCilindrada) * numDias
+		//factorCilindrada = cilindrada del turismo / 10 
+		//numDias = fechaDevolucion-fechaAlquiler
+		
+		int factorCilindrada = turismo.getCilindrada()/10;
+		Duration numDias = Duration.between(fechaDevolucion, fechaAlquiler);
+		int precio = (int)((PRECIO_DIA + factorCilindrada) * numDias.toDays());
+		
+		return precio;
+	}
 	//Métodos get y set
 	public LocalDate getFechaAlquiler() {
 		return fechaAlquiler;
@@ -39,6 +62,9 @@ public class Alquiler {
 	public void setFechaAlquiler(LocalDate fechaAlquiler) {
 		if (fechaAlquiler == null) {
 			throw new NullPointerException("La fecha NO puede ser nula.");
+		}
+		if (fechaAlquiler.isAfter(LocalDate.now())) {
+			throw new IllegalArgumentException("La fecha de alquiler no puede ser posterior a la de hoy.");
 		}
 		this.fechaAlquiler = fechaAlquiler;
 	}
@@ -51,6 +77,12 @@ public class Alquiler {
 		if (fechaDevolucion == null) {
 			throw new NullPointerException("La fecha NO puede ser nula.");
 		}
+		//La fecha de devolución no puede ser igual o anterior a la fecha de alquiler 
+		//y tampoco puede ser posterior a hoy
+		if (fechaDevolucion.isEqual(fechaAlquiler)||fechaDevolucion.isBefore(fechaAlquiler)||
+				fechaDevolucion.isAfter(LocalDate.now())) {
+			throw new IllegalArgumentException("La fecha de devolución es errónea.");
+		}
 		this.fechaDevolucion = fechaDevolucion;
 	}
 
@@ -59,6 +91,9 @@ public class Alquiler {
 	}
 
 	public void setTurismo(Turismo turismo) {
+		if (turismo == null) {
+			throw new NullPointerException("El turismo no puede ser nulo.");
+		}
 		this.turismo = turismo;
 	}
 
@@ -67,9 +102,35 @@ public class Alquiler {
 	}
 
 	public void setCliente(Cliente cliente) {
+		if (cliente == null) {
+			throw new NullPointerException("El cliente no puede ser nulo.");
+		}
 		this.cliente = cliente;
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, fechaAlquiler, fechaDevolucion, turismo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alquiler other = (Alquiler) obj;
+		return Objects.equals(cliente, other.cliente) && Objects.equals(fechaAlquiler, other.fechaAlquiler)
+				&& Objects.equals(fechaDevolucion, other.fechaDevolucion) && Objects.equals(turismo, other.turismo);
+	}
+	@Override
+	public String toString() {
+		return String.format("Alquiler [fechaAlquiler=%s, fechaDevolucion=%s, turismo=%s, cliente=%s]", fechaAlquiler,
+				fechaDevolucion, turismo, cliente);
+	}
 
+	
 
 
 }
