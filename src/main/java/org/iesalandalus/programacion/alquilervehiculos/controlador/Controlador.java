279d594f1bb260e8f7dfe4e2b8aef8cebd50b7c0
Glootie-MGF <116271650+Glootie-MGF@users.noreply.github.com>
2023-02-21 13:35:03 +0100

    Trabajando clase Controlador.

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/controlador/Controlador.java
@@ -1,5 +1,8 @@
 package org.iesalandalus.programacion.alquilervehiculos.controlador;
 
+import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+
 public class Controlador {
 
 	/*
@@ -11,4 +14,30 @@ public class Controlador {
 	 * correspondientes métodos en el modelo y en la vista. Crea los demás métodos
 	 * que simplemente harán una llamada al correspondiente método del modelo.
 	 */
+	private Modelo modelo;
+	private Vista vista;
+
+	public Controlador(Modelo modelo, Vista vista) {
+		if (modelo == null) {
+			throw new NullPointerException("ERROR: El modelo NO puede ser nulo.");
+		}
+
+		if (vista == null) {
+			throw new NullPointerException("ERROR: La vista NO puede ser nula.");
+		}
+		this.modelo = modelo;
+		this.vista = vista;
+		vista.setControlador(this);
+	}
+	
+	public void comenzar() {
+		modelo.comenzar();
+		vista.comenzar();
+	}
+
+	public void terminar() {
+		modelo.terminar();
+		vista.terminar();
+		System.out.printf("%n¡Chao Bacalao!");
+	}
 }
